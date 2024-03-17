9bfc5903af118d43e340264db585b6dbcf6e56ca
JungHyun <128463508+dkflfkd53@users.noreply.github.com>
2024-03-18 08:31:28 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    
+    for(int i =0; i<overwrite_string.length(); i++) {
+        my_string[s] =overwrite_string[i];
+        s++;
+    }
+    
+    return my_string;
+}
