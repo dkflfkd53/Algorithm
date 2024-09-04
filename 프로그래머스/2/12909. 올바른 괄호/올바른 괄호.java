a71c7868c845d6c723dd8c91db3291e291bceb69
JungHyun <128463508+dkflfkd53@users.noreply.github.com>
2024-09-04 19:02:04 +0900

    [level 2] Title: 올바른 괄호, Time: 17.73 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,26 +1,20 @@
-// 1. 문자열의 값들을 스택에 추가
-// 2. 추가하는 과정에서 )가 들어올때 (가 존재하면 2괄호 모두 삭제
-// 3. 스택이 비어있으면 true를 반환 값이 남아있으면 false를 반환
 import java.util.*;
+import java.io.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
+        if(s.length()%2==1) return false;
         
-        for(int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            
-            if(currentChar == '(') {
-                stack.push(currentChar);
-            } 
-            else if(currentChar == ')' && !stack.isEmpty() && stack.peek() == '(') {
+        Stack<String> stack = new Stack<>();
+        
+        for(int i = 0;i<s.length();i++) {
+            if(s.charAt(i) == '(') stack.push(String.valueOf(s));
+            else if(s.charAt(i) == ')') {
+                if(stack.isEmpty()) return false;
                 stack.pop();
-            }
-            else {
-                stack.push(currentChar);
             }
         }
         
-        return stack.isEmpty();
+        return (stack.isEmpty()) ? true : false;
     }
 }
